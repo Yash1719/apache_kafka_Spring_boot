47359222f05111cb6b6cb8070891ffcafb73a910
Yash1719 <yashagarwal1719@gmail.com>
2025-02-06 19:54:02 +0530

    consumer code added

---
+++ cab-book-user/src/main/java/com/apachelearning/cab_book_user/service/LocationService.java
@@ -0,0 +1,16 @@
+package com.apachelearning.cab_book_user.service;
+
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+    //create kafka listener
+    @KafkaListener(topics = "cab-location",groupId = "user-group")
+    public void cabLocation (String location)
+    {
+        System.out.println(location);
+    }
+}
+
