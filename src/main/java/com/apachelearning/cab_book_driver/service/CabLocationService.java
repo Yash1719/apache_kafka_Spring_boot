04f765e52264bd904e2dd43177c1adb5037f9155
Yash1719 <yashagarwal1719@gmail.com>
2025-02-06 19:20:46 +0530

    producer code

---
+++ src/main/java/com/apachelearning/cab_book_driver/service/CabLocationService.java
@@ -0,0 +1,19 @@
+package com.apachelearning.cab_book_driver.service;
+
+
+import com.apachelearning.cab_book_driver.constant.AppConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CabLocationService {
+@Autowired
+    private KafkaTemplate<String,Object> kafkaTemplate;
+
+public boolean updateLocation(String location)
+{
+    kafkaTemplate.send(AppConstant.CAB_LOCATION,location);
+    return true;
+}
+}
